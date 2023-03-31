8b7322f6d39230e4d6dcaf5238ae4c910630bd4e
Heisenburs <89811609+Heisenburs@users.noreply.github.com>
2023-03-31 14:18:38 -0400

    imported font

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,7 @@
 function Quote(props) {
   const { quote } = props;
   console.log(quote);
-  return <div> " {quote.quote} " - Kanye West</div>;
+  return <div className="yequote"> " {quote.quote} " - Kanye West</div>;
 }
 
 export default Quote;
